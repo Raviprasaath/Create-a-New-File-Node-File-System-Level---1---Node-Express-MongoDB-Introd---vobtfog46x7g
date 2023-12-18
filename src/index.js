022b09f57b55bc5d2a550d54a2e5a49eac045f9b
Raviprasaath <raviprasaath1@gmail.com>
2023-12-18 11:20:48 +0530

    1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+  const filePath = `./${fileName}`
+  try {
+    await fs.writeFile(filePath, fileContent);
+  } catch (e) {
+    console.log(e)
+  }
 };
 
 module.exports = { writeFile };
